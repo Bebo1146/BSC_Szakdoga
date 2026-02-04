2640599b808e61898e47fe74cbd1d02a57d36f95
Bebo1146 <mateboti09@gmail.com>
2026-02-04 22:04:51 +0100

    Add style to keyclocl login

---
+++ BE/OAuthStart/UserRegistration/Identity/IdentityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.Identity
+{
+    [ApiController]
+    [Route("api/identity")]
+    [Authorize]
+    internal class IdentityController
+    {
+    }
+}
